6d6633d4a2fe8453ca7735b3cbb6b3a71aafee60
David <davidasir2020@gmail.com>
2022-06-03 19:53:59 +0000

    chore: exercise 1 of condicionals complete

---
+++ solutions/day-01/exercise-4(cond-level-01)/main.js
@@ -0,0 +1,53 @@
+// Uncomment the tab lines in order to check the results of the code.
+// Get user input using prompt(“Enter your age:”).
+
+    // const input = prompt("Enter your age:")
+
+// If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 
+// give another feedback stating to wait for the number of years he needs to turn 18.
+
+    // if (input >= 18) {
+    //     alert("You are old enough to drive")
+    // }
+    // else 
+    // { 
+    //     alert("You are not old enough to drive, you need to wait " + (18 - input) + " years")
+    // }
+
+// Compare the values of myAge and yourAge using if … else. Based on the comparison and
+//  log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
+
+    // const myAge = 30;
+    // if (input > myAge) {
+    //     alert("You are older than me")
+    // } else {
+    //     alert("You are younger than me")
+    // }
+
+// If a is greater than b return 'a is greater than b' else 'a is less than b'. 
+// Try to implement it in two ways
+
+    // const a = prompt("Introduce A value: ");
+    // const b = prompt("Introduce B value: ");
+    // if (a > b) {
+    //     alert("A is greater than B.")
+    // } else {
+    //     alert("B is greater than A.")
+    // }
+    //
+    // const aTern = prompt("Introduce A value: ")
+    // const bTern = prompt("Introduce B value: ")
+    // aTern > bTern
+    //     ? alert("A is greater than B.")
+    //     : alert("B is greater than A.")
+
+
+// Even numbers are divisible by 2 and the remainder is zero.
+//  How do you check, if a number is even or not using JavaScript?
+
+    // const number = prompt("Introduce a number: ");
+    // number % 2 === 0
+    //     ? alert("Number is even")
+    //     : alert("Number is not even")
+
+// End of exercise.
