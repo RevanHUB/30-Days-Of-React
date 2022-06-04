8a3f262fb8719c879c4ff0196dc762e46daa3754
David <davidasir2020@gmail.com>
2022-06-04 10:32:11 +0000

    chore: exercise 3 of conds completed

---
+++ solutions/day-01/exercise-6(cond-level-03)/main.js
@@ -0,0 +1,57 @@
+// Write a program which tells the number of days in a month.
+//   Enter a month: January
+//   January has 31 days.
+
+//   Enter a month: JANUARY
+//   January has 31 day
+
+//   Enter a month: February
+//   February has 28 days.
+
+//   Enter a month: FEbruary
+//   February has 28 days.
+
+    // var date = new Date();
+    // var year = date.getFullYear();
+    // var month = date.getMonth() + 1;
+
+    // function calculateDate(year, month) {
+    //     return new Date(year, month, 0).getDate();
+    // } 
+    // function calculateMonth(month) {
+    //     return new Date(month + '-1-01').getMonth() + 1;
+    // } 
+
+    // var monthPr = prompt("Enter a month: ");
+    // var minusMonth = monthPr.toLowerCase();
+    // var filteredMonth = minusMonth[0].toUpperCase() + minusMonth.slice(1);
+    
+    // const calculatedMonth = calculateMonth(filteredMonth);
+    // const daysInMonth = calculateDate(year, calculatedMonth)
+   
+    // alert(filteredMonth + " has "+ daysInMonth + " days.")
+
+ 
+// Write a program which tells the number of days in a month, now consider leap year.
+
+    // var date = new Date();
+    // var year = date.getFullYear();
+    // var month = date.getMonth() + 1;
+
+    // function calculateDate(year, month) {
+    //     return new Date(year, month, 0).getDate();
+    // } 
+    // function calculateMonth(month) {
+    //     return new Date(month + '-1-01').getMonth() + 1;
+    // } 
+
+    // var monthPr = prompt("Enter a month: ");
+    // var minusMonth = monthPr.toLowerCase();
+    // var filteredMonth = minusMonth[0].toUpperCase() + minusMonth.slice(1);
+
+    // const calculatedMonth = calculateMonth(filteredMonth);
+    // const daysInMonth = calculateDate(2024, calculatedMonth)
+
+    // alert(filteredMonth + " has "+ daysInMonth + " days.");
+
+// End of exercise.
