0e8d014369d4f1d156ab4d871cb1175ba7481ec2
David <davidasir2020@gmail.com>
2022-06-27 22:21:12 +0000

    chore: day 3 and 4 completed

---
+++ solutions/day-04/user_login_template/server/users.js
@@ -0,0 +1,15 @@
+module.exports = users = [
+    {
+        id: 1,
+        username: 'david',
+        password: "testingPass",
+        avatar: 'https://randomuser.me/api/portraits/men/15.jpg'
+    },
+    {
+        id: 2,
+        username: 'test',
+        password: "1234",
+        avatar: 'https://randomuser.me/api/portraits/men/45.jpg',
+        techs: ['HTML','CSS', 'JavaScript'] 
+    }
+]
